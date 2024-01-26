03dc73326feec2e7758f165f34dfaa637b93cbde
lsh210726 <136397554+lsh210726@users.noreply.github.com>
2024-01-26 15:04:40 +0900

    default TPS commit

---
+++ Source/baseTPS/baseTPSGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "baseTPSGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AbaseTPSGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AbaseTPSGameMode();
+};
+
+
+
