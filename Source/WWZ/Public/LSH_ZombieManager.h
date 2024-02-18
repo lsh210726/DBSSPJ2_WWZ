2ace345e82e7c0badc3e5f736c363bb9ea99b3bf
lsh210726 <136397554+lsh210726@users.noreply.github.com>
2024-02-19 01:58:05 +0900

    Update LSH_ZombieManager.h

--- Source/WWZ/Public/LSH_ZombieManager.h
+++ Source/WWZ/Public/LSH_ZombieManager.h
@@ -47,13 +47,13 @@ public:
 	TArray<class ALSH_BaseZom*> bodyList;
 
 	UPROPERTY(EditAnywhere, Category = "zombie manager")
-	int32 zomMaxNum = 60;
+	int32 zomMaxNum = 90;
 
 	UPROPERTY(EditAnywhere, Category = "zombie manager")
-	int32 bodyMaxNum = 10;
+	int32 bodyMaxNum = 20;
 
 	UPROPERTY(EditAnywhere, Category = "zombie manager")
-	int32 queueMaxNum = 40;
+	int32 queueMaxNum = 50;
 
 	float currentTime = 0;
 
@@ -71,6 +71,5 @@ public:
 	void bodyCollecting(class ALSH_BaseZom* zombie);
 
 	//좀비가 스폰되어야 할 타이밍이면 대기열 안의 좀비들을 전부 스폰시킨다
-
 
 };
